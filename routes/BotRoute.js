e88c4229aa323af04a74cb56e68e2c3d04710d65
baploarya <aryawardhana7831@gmail.com>
2023-11-27 20:40:44 +0700

    tes

--- routes/BotRoute.js
+++ routes/BotRoute.js
@@ -10,7 +10,7 @@ const token = '6747804244:AAEb8jpi8lF8hNBghL7VRpsAQzZ-rcvgBcg';
 const bot = new TelegramBot(token, { polling: true });
 
 // Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
+bot.onText(/\/echo (.+)/, async (msg, match) => {
     // 'msg' is the received Message from Telegram
     // 'match' is the result of executing the regexp above on the text content
     // of the message
@@ -24,7 +24,7 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 // Listen for any kind of message. There are different kinds of
 // messages.
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
 
     // send a message to the chat acknowledging receipt of their message
